2b4998f338167ce1408def00ca2e134cfddf53ea
jacka7901 <jacka7901@gmail.com>
2019-10-04 16:18:33 -0400

    first version complete

---
+++ venv/Leetcode/unique_number_of_occurances.py
@@ -0,0 +1,20 @@
+def uniqueOccurrences(self, arr: List[int]) -> bool:
+    temp = []
+    s = set(arr)
+    for i in s:
+        temp.append(arr.count(i))
+
+    for x in range(len(temp) - 1):
+        for c in range(x + 1, len(temp)):
+            if temp[x] == temp[c]:
+                return False
+
+    return True
+
+
+
+
+
+
+
+
